583c4bfb1b469ee623df221ac42768bb5fe26d48
tintedsky <hongbonew@gmail.com>
2022-11-25 15:20:41 -0500

    Const int, int const, const int *, int const *, int * const, const int * const.

--- L232ConstantQualifier/main.cpp
+++ L232ConstantQualifier/main.cpp
@@ -6,9 +6,30 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
+    
+    const int x = 10;  /* const need to be intialized directly */
+    int const y = 20 ;  /* const type === type const */
+    
+    int x1 = 11, y1=21;
+    const int *p1 = &x1; /* p1 can change but (*p1) no. The value cannot be changed by the poitner */
+    cout<<p1<<endl;
+    cout<<*p1++<<endl;   /*Note the difference *p1++ vs (*p1)++ */
+    
+    p1=&y1;   /*p1 can pointer to the different memory, still 'const int *' === 'int const *' */
+    cout<<++x1<<endl;
+    /* The variable x1 itself can be changed, but cannot mo chagned through the pointer p1 */
+    
+    int x2=12, y2=22;
+    int * const p2 = &x2;
+    /** p2 = &y2; the pointer cannot point to anywhere else */
+    
+    int x3=13, y3=23;
+    const int * const p3 = &x3;
+    
     return 0;
 }
